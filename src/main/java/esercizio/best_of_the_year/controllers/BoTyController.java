2f3c05c13a13a02e4b555cf77c4e805bf0e79d91
CristianMastro <cristian.mastro92@outlook.it>
2025-03-28 18:26:01 +0100

    feat: aggiunti /movies e /film

--- src/main/java/esercizio/best_of_the_year/controllers/BoTyController.java
+++ src/main/java/esercizio/best_of_the_year/controllers/BoTyController.java
@@ -26,12 +26,37 @@ public class BoTyController {
         return songs;   
     }
 
+
 @GetMapping("/bestOfTheYear")
 public String bestOfTheYear(Model model, @RequestParam(name = "name") String name) {
     model.addAttribute("name", name);
     model.addAttribute("movies", getBestMovies());
     model.addAttribute("songs", getBestSongs());
     return "bestOfTheYear";
+}
+
+@GetMapping("/movies")
+public String movies(Model model, @RequestParam(name = "name") String name) {
+
+    Movie f1 = new Movie(1, "Heretic");
+    Movie f2 = new Movie(2, "Follemente");
+
+    model.addAttribute("name", name);
+    model.addAttribute("f1", f1);
+    model.addAttribute("f2", f2);
+    return "movies";
+}
+
+@GetMapping("/songs")
+public String songs(Model model, @RequestParam(name = "name") String name) {
+
+    Song s1 = new Song(1, "ReawakeR - Lisa");
+    Song s2 = new Song(1, "Shadowborn - Sawano Hiroyuki");
+
+    model.addAttribute("name", name);
+    model.addAttribute("s1", s1); 
+    model.addAttribute("s2", s2); 
+    return "songs";
 }
 
 
