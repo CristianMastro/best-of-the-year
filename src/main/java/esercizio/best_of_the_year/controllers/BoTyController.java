7a20a1e35ad612499e1170b852abf7df5daba381
CristianMastro <cristian.mastro92@outlook.it>
2025-03-28 01:12:56 +0100

    feat:aggiunti metodi liste

--- src/main/java/esercizio/best_of_the_year/controllers/BoTyController.java
+++ src/main/java/esercizio/best_of_the_year/controllers/BoTyController.java
@@ -1,18 +1,38 @@
 package esercizio.best_of_the_year.controllers;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import esercizio.best_of_the_year.models.Movie;
+import esercizio.best_of_the_year.models.Song;
+
 @Controller
 @RequestMapping("/")
 public class BoTyController {
+
+    private List<Movie> movies;
+    private List<Song> songs;
+
+    private List<Movie> getBestMovies() {
+        return movies;
+    }
+
+    private List<Song> getBestSongs() {
+        return songs;   
+    }
+
 @GetMapping("/bestOfTheYear")
-public String greeting(Model model, @RequestParam(name = "name") String name) {
+public String bestOfTheYear(Model model, @RequestParam(name = "name") String name) {
     model.addAttribute("name", name);
+    model.addAttribute("movies", getBestMovies());
+    model.addAttribute("songs", getBestSongs());
     return "bestOfTheYear";
 }
+
 
 }
---
+++ src/main/java/esercizio/best_of_the_year/models/Song.java
@@ -0,0 +1,26 @@
+package esercizio.best_of_the_year.models;
+
+public class Song {
+    
+    private int id;
+    private String titolo;
+
+    public Song(int id, String titolo) {
+        this.id = id;
+        this.titolo = titolo;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getTitolo() {
+        return titolo;
+    }
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+    
+}
