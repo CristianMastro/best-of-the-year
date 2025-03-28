dcfd4f8771cae331b05d1dfb66350658039927e4
CristianMastro <cristian.mastro92@outlook.it>
2025-03-28 21:38:53 +0100

    feat: aggiunti metodi con le liste

--- src/main/java/esercizio/best_of_the_year/controllers/BoTyController.java
+++ src/main/java/esercizio/best_of_the_year/controllers/BoTyController.java
@@ -1,6 +1,8 @@
 package esercizio.best_of_the_year.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,14 +17,18 @@ import esercizio.best_of_the_year.models.Song;
 @RequestMapping("/")
 public class BoTyController {
 
-    private List<Movie> movies;
-    private List<Song> songs;
 
     private List<Movie> getBestMovies() {
+        List<Movie> movies = new ArrayList<>();
+        movies.add(new Movie(1, "Heretic"));
+        movies.add(new Movie(2, "Follemente"));
         return movies;
     }
 
     private List<Song> getBestSongs() {
+        List<Song> songs = new ArrayList<>();
+        songs.add(new Song(1, "ReawakeR - Lisa"));
+        songs.add(new Song(2, "Shadowborn - Sawano Hiroyuki"));
         return songs;   
     }
 
@@ -30,9 +36,29 @@ public class BoTyController {
 @GetMapping("/bestOfTheYear")
 public String bestOfTheYear(Model model, @RequestParam(name = "name") String name) {
     model.addAttribute("name", name);
-    model.addAttribute("movies", getBestMovies());
-    model.addAttribute("songs", getBestSongs());
     return "bestOfTheYear";
+}
+
+@GetMapping("/moviesList")
+public String moviesList(Model model, @RequestParam(name = "name") String name) {
+
+    List<Movie> bestMovies = getBestMovies();
+    String movieTitles = bestMovies.stream().map(Movie::getTitolo).collect(Collectors.joining(", "));
+
+    model.addAttribute("name", name);
+    model.addAttribute("movieTitles", movieTitles);
+    return "moviesList";
+}
+
+@GetMapping("/songList")
+public String songsList(Model model, @RequestParam(name = "name") String name) {
+
+    List<Song> bestSongs = getBestSongs();
+    String songTitles = bestSongs.stream().map(Song::getTitolo).collect(Collectors.joining(", "));
+
+    model.addAttribute("name", name);
+    model.addAttribute("bestSong", songTitles);
+    return "songList";
 }
 
 @GetMapping("/movies")
@@ -51,7 +77,7 @@ public String movies(Model model, @RequestParam(name = "name") String name) {
 public String songs(Model model, @RequestParam(name = "name") String name) {
 
     Song s1 = new Song(1, "ReawakeR - Lisa");
-    Song s2 = new Song(1, "Shadowborn - Sawano Hiroyuki");
+    Song s2 = new Song(2, "Shadowborn - Sawano Hiroyuki");
 
     model.addAttribute("name", name);
     model.addAttribute("s1", s1); 
